2aa5e845223673b3c440612c76d4b081ce0a82a6
Camilo_WH <yefersoncamiloblanco@gmail.com>
2025-08-22 17:20:43 -0500

    Cambios

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();("Hola causa");
-        System.out.println();("Yeferson camilo Blanco");
-        System.out.println();("iNG EN SISTEMAS ");
-
+        System.out.println("Hola causa");
+        System.out.println("Yeferson camilo Blanco");
+        System.out.println("iNG EN SISTEMAS ");
+        System.out.println("iNG EN SISTEMAS ");
+        System.out.println("iNG EN SISTEMAS ");
     }
 }
